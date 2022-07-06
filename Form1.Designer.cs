58444c92548166948dc79bf567fd4553b1678be8
tvlad1234 <vlad.tomoiaga@gmail.com>
2022-07-06 17:31:28 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Go!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "COM Port";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(158, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(335, 399);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 295);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 21);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Rolling";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Serial port monitor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(15, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 138);
+            this.panel1.TabIndex = 15;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ref1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ref2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ref3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 101);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Ref4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Destination";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 449);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "TekScope waveform ingest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp3
+{   
+
+    public partial class Form1 : Form
+    {
+        static TcpClient client;
+        static NetworkStream stream;
+        static bool receiving = false, listening = false;
+        static string payload;
+        System.IO.StreamWriter writer;
+
+        static int lineNum, atten;
+        static double sampPer, offsetVoltage;
+
+        void UpdateWave()
+        {
+            string message = "RECALL:WAVEFORM \"data.csv\",";
+            if (radioButton1.Checked)
+                message += "REF1\n";
+            else if (radioButton2.Checked)
+                message += "REF2\n";
+            else if (radioButton3.Checked)
+                message += "REF3\n";
+            else if (radioButton4.Checked)
+                message += "REF4\n";
+           // MessageBox.Show(message);
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            try
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lost connection to TekScope!");
+                Application.Exit();
+            }
+        }
+
+        void SetTekScopePath()
+        {
+
+            String message = "FILESystem:CWD \"" + Directory.GetCurrentDirectory() + "\"\n";
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            try
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lost connection to TekScope!");
+                Application.Exit();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listening = false;
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            receiving = false;
+            label3.Text = "Ready";
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.BytesToRead > 6 && listening)
+            {
+                string str = serialPort1.ReadLine();
+                richTextBox1.Text += str;
+                if (str.Equals("BeginWave!"))
+                {
+                    label3.Text = "Receiving";
+                    payload = "";
+                    lineNum = 0;
+                    receiving = true;
+                    payload += "Model,TekscopeSW\n\r";
+                    payload += "Label,CH1\n\r";
+                    payload += "Waveform Type,ANALOG\n\r";
+                    payload += "Horizontal Units, s\n\r";
+
+                }
+                else if (str.Equals("\rSendWaveComplete!"))
+                {
+                    writer = new System.IO.StreamWriter("data.csv");
+                    writer.Write(payload);
+                    writer.Close();
+                    UpdateWave();
+
+                    if (checkBox1.Checked && listening)
+                    {
+                        richTextBox1.Clear();
+                        serialPort1.DiscardInBuffer();
+                        serialPort1.Write("       S");
+                    }
+                    else
+                    {
+                        receiving = false;
+                        label3.Text = "Ready";
+                        serialPort1.Close();
+                    }
+                }
+                else
+                {
+                    str = str.Remove(0, 1);
+                    if (lineNum == 0) //SampPer
+                    {
+                        sampPer = Convert.ToDouble(str);
+                        payload += "Sample Interval," + str + "E-06\n\r";
+
+                    }
+                    else if (lineNum == 1) //RecordLength
+                    {
+                        payload += "Record Length," + str + "\n\r";
+                    }
+                    else if (lineNum == 2)
+                    {
+                        payload += "Zero Index," + str + "\n\r";
+
+                        payload += "Vertical Units, V\n\r";
+                        payload += ",\n\rLabels,\n\r";
+                        payload += "TIME,CH1\n\r";
+
+                    }
+                    else if (lineNum == 3)
+                    {
+                        offsetVoltage = Convert.ToDouble(str);
+                    }
+
+                    else if (lineNum == 4)
+                    {
+                        atten = Convert.ToInt32(str);
+                    }
+
+                    else
+                    {
+                        string timeStamp = Convert.ToString((lineNum - 2) * sampPer);
+                        int samp = Convert.ToInt32(str);
+                        double voltage = 2 * atten * ((3.3 * samp / 4096.0) - offsetVoltage);
+                        payload += timeStamp + "E-06," + voltage.ToString() + "\n\r";
+                    }
+                    lineNum++;
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = comboBox1.Text;
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Couldn't open serial port!");
+                }
+
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                SetTekScopePath();
+                serialPort1.DiscardInBuffer();
+                richTextBox1.Clear();
+                serialPort1.Write("S");
+                label3.Text = "Waiting for data";
+                listening = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient("localhost", 4000);
+                stream = client.GetStream();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't connect to TekScope!");
+                Application.Exit();
+            }
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = 0;
+            radioButton1.Checked = true;
+            label3.Text = "Ready";
+        }
+
+    }
+}
